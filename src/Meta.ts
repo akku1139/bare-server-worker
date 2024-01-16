d99dbf08696aa1d076a612fd46e1b50afbe528d6
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-16 14:29:22 +0900

    wip

--- src/Meta.ts
+++ src/Meta.ts
@@ -1,12 +1,5 @@
 import type { BareHeaders, BareRemote } from './requestUtil';
 
-export interface MetaV1 {
-	v: 1;
-	response?: {
-		headers: BareHeaders;
-	};
-}
-
 export interface MetaV2 {
 	v: 2;
 	response?: { status: number; statusText: string; headers: BareHeaders };
@@ -16,7 +9,7 @@ export interface MetaV2 {
 }
 
 export default interface CommonMeta {
-	value: MetaV1 | MetaV2;
+	value:  MetaV2;
 	expires: number;
 }
 
